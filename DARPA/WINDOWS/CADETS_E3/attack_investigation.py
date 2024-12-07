225bc3988aec1f365a6bd53c74ab094a220cd656
fairuz-shaiara <shaiarafairuz@gmail.com>
2024-12-07 02:20:41 -0600

    Add comments to attack_investigation.py (CadetsE3)

--- DARPA/WINDOWS/CADETS_E3/attack_investigation.py
+++ DARPA/WINDOWS/CADETS_E3/attack_investigation.py
@@ -1,32 +1,30 @@
 import os
-
 from graphviz import Digraph
 import networkx as nx
-import datetime
 import community.community_louvain as community_louvain
 from tqdm import tqdm
 
-from config import *
-from kairos_utils import *
+from config import *  # Configuration settings
+from kairos_utils import *  # Utility functions
 
-
-# Some common path abstraction for visualization
+# Common path abstractions to simplify file paths for visualization
 replace_dic = {
-    '/run/shm/': '/run/shm/*',
-    '/home/admin/.cache/mozilla/firefox/': '/home/admin/.cache/mozilla/firefox/*',
-    '/home/admin/.mozilla/firefox': '/home/admin/.mozilla/firefox*',
-    '/data/replay_logdb/': '/data/replay_logdb/*',
-    '/home/admin/.local/share/applications/': '/home/admin/.local/share/applications/*',
-    '/usr/share/applications/': '/usr/share/applications/*',
-    '/lib/x86_64-linux-gnu/': '/lib/x86_64-linux-gnu/*',
-    '/proc/': '/proc/*',
-    '/stat': '*/stat',
-    '/etc/bash_completion.d/': '/etc/bash_completion.d/*',
-    '/usr/bin/python2.7': '/usr/bin/python2.7/*',
-    '/usr/lib/python2.7': '/usr/lib/python2.7/*',
+    "/run/shm/": "/run/shm/*",
+    "/home/admin/.cache/mozilla/firefox/": "/home/admin/.cache/mozilla/firefox/*",
+    "/home/admin/.mozilla/firefox": "/home/admin/.mozilla/firefox*",
+    "/data/replay_logdb/": "/data/replay_logdb/*",
+    "/home/admin/.local/share/applications/": "/home/admin/.local/share/applications/*",
+    "/usr/share/applications/": "/usr/share/applications/*",
+    "/lib/x86_64-linux-gnu/": "/lib/x86_64-linux-gnu/*",
+    "/proc/": "/proc/*",
+    "/stat": "*/stat",
+    "/etc/bash_completion.d/": "/etc/bash_completion.d/*",
+    "/usr/bin/python2.7": "/usr/bin/python2.7/*",
+    "/usr/lib/python2.7": "/usr/lib/python2.7/*",
 }
 
 
+# Replace specific file paths with generalized names for better readability
 def replace_path_name(path_name):
     for i in replace_dic:
         if i in path_name:
@@ -34,61 +32,99 @@ def replace_path_name(path_name):
     return path_name
 
 
-# Users should manually put the detected anomalous time windows here
+# Replace invalid characters in filenames with underscores for cross-platform compatibility
+def sanitize_filename(filename):
+    return filename.translate(str.maketrans({":": "_", "\\": "_", "/": "_"}))
+
+
+# Manually detected anomalous time windows (files containing edge data)
 attack_list = [
-    artifact_dir+'/graph_4_6/2018-04-06 11:18:26.126177915~2018-04-06 11:33:35.116170745.txt',
-    artifact_dir+'/graph_4_6/2018-04-06 11:33:35.116170745~2018-04-06 11:48:42.606135188.txt',
-    artifact_dir+'/graph_4_6/2018-04-06 11:48:42.606135188~2018-04-06 12:03:50.186115455.txt',
-    artifact_dir+'/graph_4_6/2018-04-06 12:03:50.186115455~2018-04-06 14:01:32.489584227.txt',
+    os.path.join(
+        artifact_dir,
+        "graph_4_6",
+        sanitize_filename(
+            "2018-04-06 11:18:26.126177915~2018-04-06 11:33:35.116170745.txt"
+        ),
+    ),
+    os.path.join(
+        artifact_dir,
+        "graph_4_6",
+        sanitize_filename(
+            "2018-04-06 11:33:35.116170745~2018-04-06 11:48:42.606135188.txt"
+        ),
+    ),
+    os.path.join(
+        artifact_dir,
+        "graph_4_6",
+        sanitize_filename(
+            "2018-04-06 11:48:42.606135188~2018-04-06 12:03:50.186115455.txt"
+        ),
+    ),
+    os.path.join(
+        artifact_dir,
+        "graph_4_6",
+        sanitize_filename(
+            "2018-04-06 12:03:50.186115455~2018-04-06 14:01:32.489584227.txt"
+        ),
+    ),
 ]
 
+# Initialize variables for storing graphs and edges
+original_edges_count = 0  # Count of total edges in the original data
+graphs = []  # List of individual graphs
+gg = nx.DiGraph()  # Combined directed graph
+count = 0  # Overall edge count
 
-original_edges_count = 0
-graphs = []
-gg = nx.DiGraph()
-count = 0
+# Process each file in the anomalous time windows
 for path in tqdm(attack_list):
-    if ".txt" in path:
+    if ".txt" in path:  # Ensure the file is a text file
         line_count = 0
         node_set = set()
-        tempg = nx.DiGraph()
-        f = open(path, "r")
-        edge_list = []
-        for line in f:
-            count += 1
-            l = line.strip()
-            jdata = eval(l)
-            edge_list.append(jdata)
+        tempg = nx.DiGraph()  # Temporary graph for the current file
+        with open(path, "r") as f:
+            edge_list = []
+            for line in f:
+                count += 1
+                l = line.strip()
+                jdata = eval(l)  # Evaluate the line to get edge data
+                edge_list.append(jdata)
 
-        edge_list = sorted(edge_list, key=lambda x: x['loss'], reverse=True)
+        # Sort edges by their loss values (descending)
+        edge_list = sorted(edge_list, key=lambda x: x["loss"], reverse=True)
         original_edges_count += len(edge_list)
 
-        loss_list = []
-        for i in edge_list:
-            loss_list.append(i['loss'])
+        # Calculate threshold for identifying high-loss edges
+        loss_list = [e["loss"] for e in edge_list]
         loss_mean = mean(loss_list)
         loss_std = std(loss_list)
+        thr = loss_mean + 1.5 * loss_std  # Threshold: Mean + 1.5 * Std
         print(loss_mean)
         print(loss_std)
-        thr = loss_mean + 1.5 * loss_std
         print("thr:", thr)
+
+        # Add high-loss edges to the graph
         for e in edge_list:
-            if e['loss'] > thr:
-                tempg.add_edge(str(hashgen(replace_path_name(e['srcmsg']))),
-                               str(hashgen(replace_path_name(e['dstmsg']))))
-                gg.add_edge(str(hashgen(replace_path_name(e['srcmsg']))), str(hashgen(replace_path_name(e['dstmsg']))),
-                            loss=e['loss'], srcmsg=e['srcmsg'], dstmsg=e['dstmsg'], edge_type=e['edge_type'],
-                            time=e['time'])
+            if e["loss"] > thr:
+                tempg.add_edge(
+                    str(hashgen(replace_path_name(e["srcmsg"]))),
+                    str(hashgen(replace_path_name(e["dstmsg"]))),
+                )
+                gg.add_edge(
+                    str(hashgen(replace_path_name(e["srcmsg"]))),
+                    str(hashgen(replace_path_name(e["dstmsg"]))),
+                    loss=e["loss"],
+                    srcmsg=e["srcmsg"],
+                    dstmsg=e["dstmsg"],
+                    edge_type=e["edge_type"],
+                    time=e["time"],
+                )
 
-
+# Apply community detection to group nodes into communities
 partition = community_louvain.best_partition(gg.to_undirected())
 
-# Generate the candidate subgraphs based on community discovery results
+# Generate subgraphs based on community detection results
 communities = {}
-max_partition = 0
-for i in partition:
-    if partition[i] > max_partition:
-        max_partition = partition[i]
+max_partition = max(partition.values())
 for i in range(max_partition + 1):
     communities[i] = nx.DiGraph()
 for e in gg.edges:
@@ -96,85 +132,87 @@ for e in gg.edges:
     communities[partition[e[1]]].add_edge(e[0], e[1])
 
 
-# Define the attack nodes. They are **only be used to plot the colors of attack nodes and edges**.
-# They won't change the detection results.
+# Define attack nodes for visualization purposes
 def attack_edge_flag(msg):
     attack_nodes = [
-        '/tmp/vUgefal',
-        'vUgefal',
-        '/var/log/devc',
-        '/etc/passwd',
-        '81.49.200.166',
-        '61.167.39.128',
-        '78.205.235.65',
-        '139.123.0.113',
+        "/tmp/vUgefal",
+        "vUgefal",
+        "/var/log/devc",
+        "/etc/passwd",
+        "81.49.200.166",
+        "61.167.39.128",
+        "78.205.235.65",
+        "139.123.0.113",
         "'nginx'",
     ]
-    flag = False
-    for i in attack_nodes:
-        if i in msg:
-            flag = True
-    return flag
+    return any(i in msg for i in attack_nodes)
 
 
-# Plot and render candidate subgraph
-os.system(f"mkdir -p {artifact_dir}/graph_visual/")
+# Render and save subgraphs as PDF files
+graph_visual_path = os.path.join(artifact_dir, "graph_visual")
+os.makedirs(graph_visual_path, exist_ok=True)
+
 graph_index = 0
 for c in communities:
     dot = Digraph(name="MyPicture", comment="the test", format="pdf")
-    dot.graph_attr['rankdir'] = 'LR'
+    dot.graph_attr["rankdir"] = "LR"
 
     for e in communities[c].edges:
         try:
             temp_edge = gg.edges[e]
-            srcnode = e['srcnode']
-            dstnode = e['dstnode']
-        except:
-            pass
+        except KeyError:
+            continue
 
-        if True:
-            # source node
-            if "'subject': '" in temp_edge['srcmsg']:
-                src_shape = 'box'
-            elif "'file': '" in temp_edge['srcmsg']:
-                src_shape = 'oval'
-            elif "'netflow': '" in temp_edge['srcmsg']:
-                src_shape = 'diamond'
-            if attack_edge_flag(temp_edge['srcmsg']):
-                src_node_color = 'red'
-            else:
-                src_node_color = 'blue'
-            dot.node(name=str(hashgen(replace_path_name(temp_edge['srcmsg']))), label=str(
-                replace_path_name(temp_edge['srcmsg']) + str(
-                    partition[str(hashgen(replace_path_name(temp_edge['srcmsg'])))])), color=src_node_color,
-                     shape=src_shape)
+        # Source node visualization
+        if "'subject': '" in temp_edge["srcmsg"]:
+            src_shape = "box"
+        elif "'file': '" in temp_edge["srcmsg"]:
+            src_shape = "oval"
+        elif "'netflow': '" in temp_edge["srcmsg"]:
+            src_shape = "diamond"
+        src_node_color = "red" if attack_edge_flag(temp_edge["srcmsg"]) else "blue"
+        dot.node(
+            name=str(hashgen(replace_path_name(temp_edge["srcmsg"]))),
+            label=str(
+                replace_path_name(temp_edge["srcmsg"])
+                + str(partition[str(hashgen(replace_path_name(temp_edge["srcmsg"])))]),
+            ),
+            color=src_node_color,
+            shape=src_shape,
+        )
 
-            # destination node
-            if "'subject': '" in temp_edge['dstmsg']:
-                dst_shape = 'box'
-            elif "'file': '" in temp_edge['dstmsg']:
-                dst_shape = 'oval'
-            elif "'netflow': '" in temp_edge['dstmsg']:
-                dst_shape = 'diamond'
-            if attack_edge_flag(temp_edge['dstmsg']):
-                dst_node_color = 'red'
-            else:
-                dst_node_color = 'blue'
-            dot.node(name=str(hashgen(replace_path_name(temp_edge['dstmsg']))), label=str(
-                replace_path_name(temp_edge['dstmsg']) + str(
-                    partition[str(hashgen(replace_path_name(temp_edge['dstmsg'])))])), color=dst_node_color,
-                     shape=dst_shape)
+        # Destination node visualization
+        if "'subject': '" in temp_edge["dstmsg"]:
+            dst_shape = "box"
+        elif "'file': '" in temp_edge["dstmsg"]:
+            dst_shape = "oval"
+        elif "'netflow': '" in temp_edge["dstmsg"]:
+            dst_shape = "diamond"
+        dst_node_color = "red" if attack_edge_flag(temp_edge["dstmsg"]) else "blue"
+        dot.node(
+            name=str(hashgen(replace_path_name(temp_edge["dstmsg"]))),
+            label=str(
+                replace_path_name(temp_edge["dstmsg"])
+                + str(partition[str(hashgen(replace_path_name(temp_edge["dstmsg"])))]),
+            ),
+            color=dst_node_color,
+            shape=dst_shape,
+        )
 
-            if attack_edge_flag(temp_edge['srcmsg']) and attack_edge_flag(temp_edge['dstmsg']):
-                edge_color = 'red'
-            else:
-                edge_color = 'blue'
-            dot.edge(str(hashgen(replace_path_name(temp_edge['srcmsg']))),
-                     str(hashgen(replace_path_name(temp_edge['dstmsg']))), label=temp_edge['edge_type'],
-                     color=edge_color)
+        # Edge visualization
+        edge_color = (
+            "red"
+            if attack_edge_flag(temp_edge["srcmsg"])
+            and attack_edge_flag(temp_edge["dstmsg"])
+            else "blue"
+        )
+        dot.edge(
+            str(hashgen(replace_path_name(temp_edge["srcmsg"]))),
+            str(hashgen(replace_path_name(temp_edge["dstmsg"]))),
+            label=temp_edge["edge_type"],
+            color=edge_color,
+        )
 
-    dot.render(f'{artifact_dir}/graph_visual/subgraph_' + str(graph_index), view=False)
+    # Save the subgraph to a file
+    dot.render(os.path.join(graph_visual_path, f"subgraph_{graph_index}"), view=False)
     graph_index += 1
-
-
-
